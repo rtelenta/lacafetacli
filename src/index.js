c2a379219766ec4867d7233cee2d5232a5ffdf5c
Renzo Telenta <renzotelenta@gmail.com>
2019-06-05 14:48:07 -0500

    feat: cli

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const inquirer = require("inquirer");
+
+(async () => {
+  const { framework } = await inquirer.prompt([
+    {
+      type: "list",
+      message: "Pick the framework you want to use:",
+      name: "framework",
+      choices: ["react", "vue"]
+    }
+  ]);
+
+  console.log(`Installing ${framework} project`);
+})();
